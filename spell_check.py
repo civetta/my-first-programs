34c734e6ac58ade3854f7f6c3c9e6ac220919c03
civetta <civetta.xx@gmail.com>
2013-07-06 19:55:16 -0400

    Completed with comments

--- spell_check.py
+++ spell_check.py
@@ -1,15 +1,22 @@
 def spell_check(n):
-  #Declaring variables and preparing the string to alter
+  #Declaring variables and opening the dictionary
   mispelled_word=[]
   dictionary = open("DictionaryE.txt").readlines()
+  #Sending the string to all lower cases letters and seperating it into a list
   str=n.lower().split()
   str=list(str)
+  #removing the commas or anything else from the string list
+  for i in range(len(str)):
+     str[i]=str[i].strip()
+  #removing the return characters at the end of the list
   for i in range (len(dictionary)):
     dictionary[i]=dictionary[i].strip()
+  #for each word in the sentence, if it's not in the dictionary then append it to the mispelled_word list
   for e in str:
      if e not in dictionary:
           mispelled_word.append(e)
           continue
+  #run the list and using the jaccord coeffictient checks how close the words in the dictionary is compared to the mispelled word
   for e in mispelled_word:
     mispelled=e
     a=set(e)   
